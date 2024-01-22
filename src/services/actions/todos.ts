85b1347c69d5ee93c0885e1eda486d6314d4fef4
K0nstant1nS <K1kollov@gmail.com>
2024-01-22 17:48:56 +0300

    feat: root

--- src/services/actions/todos.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from "@reduxjs/toolkit"
-
-export const ADD_TODO = createAction('todos/ADD_TODO');
-export const CHANGE_TODO = createAction('todos/CHANGE_TODO');
-export const REMOVE_TODO = createAction('todos/REMOVE_TODO');
--- src/services/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { TypedUseSelectorHook, useSelector as selectorHook, useDispatch as dispatchHook  } from "react-redux";
-import {ThunkDispatch} from 'redux-thunk'
-import { AnyAction } from "redux";
-import { RootState } from "./store";
-
-
-export type TThunkDispatch = ThunkDispatch<RootState, any, AnyAction>;
-export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
-export const useDispatch = ()=> dispatchHook<TThunkDispatch>()
